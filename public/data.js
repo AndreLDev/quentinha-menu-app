dfc45f97733c9b7316aa621dc59a9cc308713fcb
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2024-01-26 16:29:14 -0300

    first commit

---
+++ public/data.js
@@ -0,0 +1,184 @@
+import React from "react";
+
+export const Menus = [
+    {
+        title: 'Almoço',
+        itens: [
+            {
+                title: "Contra Filé",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202311241957_pyIb_.jpeg",
+                price: "R$ 45,00",
+            },
+            {
+                title: "Parmegiana de Bife",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202311232056_XhIF_.jpeg",
+                price: "R$ 47,50",
+            },
+            {
+                title: "Parmegiana de Frango",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202311230112_HWFD_.jpeg",
+                price: "R$ 40,00",
+            },
+            {
+                title: "Bisteca Suína",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202311230107_58kN_.jpeg",
+                price: "R$ 32,00",
+            },
+            {
+                title: "Calabresa Fresca",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202311222345_TFHp_.jpeg",
+                price: "R$ 32,00",
+            },
+            {
+                title: "Filé de Frango",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202311230059_TQaA_.jpeg",
+                price: "R$ 32,00",
+            },
+            {
+                title: "Bife de Contra Filé",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202312081822_YH6P_.jpeg",
+                price: "R$ 35,00",
+            },
+            {
+                title: "Omelete",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/4b5b411c-e602-492c-a554-3304fede2d4d/202312292252_tezr_.jpeg",
+                price: "R$ 32,00",
+            },
+        ]
+    },
+    {
+        title: 'Jantar',
+        itens: [
+            {
+                title: "Cuscuz Carne Seca",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/d747f206-c6c3-4687-9390-a6691f4a59b8/202303311330_78K5_i.jpg",
+                price: "R$ 38,50",
+            },
+            {
+                title: "Cuscuz 3 Queijos",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/d747f206-c6c3-4687-9390-a6691f4a59b8/202304021651_0XPV_i.jpg",
+                price: "R$ 36,50",
+            },
+            {
+                title: "Cuscuz Ovo e Calabresa",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/d747f206-c6c3-4687-9390-a6691f4a59b8/202301062023_N0H2_i.jpg",
+                price: "R$ 38,50",
+            },
+            {
+                title: "Cuscuz Ovo e Bacon",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/d747f206-c6c3-4687-9390-a6691f4a59b8/202303311412_F4IV_i.jpg",
+                price: "R$ 38,50",
+            },
+            {
+                title: "Tapioca Calabresa com Queijo",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/d747f206-c6c3-4687-9390-a6691f4a59b8/202304011437_4AD6_i.jpg",
+                price: "R$ 18,50",
+            },
+            {
+                title: "Tqpioca Ovo, Presunto e Queijo",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/d747f206-c6c3-4687-9390-a6691f4a59b8/202401122302_O0JQ_i.jpg",
+                price: "R$ 23,00",
+            },
+            {
+                title: "Tapioca Frango com Catupiry",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/d747f206-c6c3-4687-9390-a6691f4a59b8/202304021704_G55C_i.jpg",
+                price: "R$ 19,50",
+            },
+            {
+                title: "Arroz Carreteiro",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/0ac63075-e5d3-4037-bae5-293b8afaac37/202208092101_685P_i.jpg",
+                price: "R$ 52,90",
+            },
+        ]
+    },
+    {
+        title: 'Bebidas',
+        itens: [
+            {
+                title: "Coca-Cola",
+                img: "https://tezeio.vtexassets.com/arquivos/ids/1661537/7894900010015_1_1.png?v=638379866603870000",
+                price: "R$4,99",
+            },
+            {
+                title: "Coca-Cola Zero",
+                img: "https://coopsp.vtexassets.com/arquivos/ids/217932-1200-1200?v=637919541979200000&width=1200&height=1200&aspect=true",
+                price: "R$5,00",
+            },
+            {
+                title: "Pepsi Cola",
+                img: "https://cdn.awsli.com.br/2500x2500/1330/1330028/produto/51587140/d638c6af0a.jpg",
+                price: "R$2,47",
+            },
+            {
+                title: "Pepsi Zero",
+                img: "https://img.megaboxatacado.com.br/produto/1000X1000/2017712_83360.jpg",
+                price: "R$2,79",
+            },
+            {
+                title: "Guaraná",
+                img: "https://cdn.awsli.com.br/2500x2500/1330/1330028/produto/51587137/a19f18b3f5.jpg",
+                price: "R$3,39",
+            },
+            {
+                title: "Fanta",
+                img: "https://ferrosdoces.fbitsstatic.net/img/p/refrigerante-fanta-laranja-lata-220ml-71914/258426.jpg?w=420&h=420&v=no-change&qs=ignore",
+                price: "R$5,00",
+            },
+            {
+                title: "Matte Leão",
+                img: "https://media.soujusto.com.br/products/7891098040602.jpg",
+                price: "R$7,19",
+            },
+            {
+                title: "Sprite",
+                img: "https://mambodelivery.vtexassets.com/arquivos/ids/179025-800-450?v=637883919659400000&width=800&height=450&aspect=true",
+                price: "R$3,69",
+            },
+        ]
+    },
+    {
+        title: 'Sobremesas',
+        itens: [
+            {
+                title: "Bolo Pote Ninho Nutella",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202301270218_4T41_i.jpg",
+                price: "R$ 22,99",
+            },
+            {
+                title: "Bolo Pote Prestígio",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202301270222_B520_i.jpg",
+                price: "R$ 22,99",
+            },
+            {
+                title: "Brownie",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202301211903_5XI6_i.jpg",
+                price: "R$ 14,99",
+            },
+            {
+                title: "Trufas",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202304011938_8IBM_i.jpg",
+                price: "R$ 6,99",
+            },
+            {
+                title: "Espeto de Morango",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202301212051_W64H_i.jpg",
+                price: "$15.70",
+            },
+            {
+                title: "Copo Bis",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202308101646_WA87_i.jpg",
+                price: "R$ 29,99",
+            },
+            {
+                title: "Copo Morango",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202301262237_TCRB_i.jpg",
+                price: "R$ 29,99",
+            },
+            {
+                title: "Tortinha de Limão",
+                img: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/33d9273b-4240-4ce9-9612-66010f764e4f/202301270211_0LWE_i.jpg",
+                price: "R$ 16,90",
+            },
+        ]
+    },
+];
